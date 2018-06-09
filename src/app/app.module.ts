3d94f819dc2bc359317b44a1399836c150210859
fodlaci-git <fodorl@me.com>
2018-06-09 22:49:14 +0200

    caruasel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,25 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
-
+import {MatGridListModule} from '@angular/material/grid-list';
 import { MatButtonModule, MatIconModule, MatInputModule, MatSelectModule, MatSliderModule, MatMenuModule,
          MatToolbarModule, MatCardModule, MatListModule, MatSlideToggleModule, MatSidenavModule } from "@angular/material";
 
 import { NgxCarouselModule } from 'ngx-carousel';
+import { Ng2CarouselamosModule } from 'ng2-carouselamos';
 
 import 'hammerjs';
 import { HomeComponent } from './pages/home/home.component';
 import { TicketComponent } from './pages/ticket/ticket.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { CourseComponent } from './pages/course/course.component';
 
 
 const routerSettings: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent   },
   { path: 'ticket', component: TicketComponent  },
-  { path: 'poolbar', component: HomeComponent  },
+  { path: 'course', component: CourseComponent  },
   { path: 'service', component: HomeComponent  },
   { path: 'contakt', component: HomeComponent  },
   { path: 'season', component: HomeComponent  },
@@ -36,12 +38,15 @@ const routerSettings: Routes = [
     AppComponent,
     HomeComponent,
     TicketComponent,
-    CarouselComponent
+    CarouselComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     NgxCarouselModule,
+    Ng2CarouselamosModule,
+    MatGridListModule,
     MatButtonModule, MatIconModule, MatInputModule, MatSelectModule, MatSliderModule,
     MatToolbarModule, MatCardModule, MatListModule, MatSlideToggleModule, MatSidenavModule, MatMenuModule,
     RouterModule.forRoot(routerSettings)
--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -22,20 +22,21 @@ export class CarouselComponent implements OnInit {
       speed: 400,
       interval: 10000,
       point: {
-        visible: true
+        visible: false
       },
       loop: true,
       touch: true
     }
+
 
    }
 
   ngOnInit() {
 
   	 const szov1 = "Az uszoda minen vasárnap 19:00 óráig várja Önöket";
-    this.items.push({title: szov1,color:"blue"});
-    this.items.push({title:"slide 2",color:"red"});
-    this.items.push({title:"slide 3",color:"orange"});
+    this.items.push({title: szov1,color:"blue",tartalom: "Ez a tartalmi rész", foto: "http://fodlaci.hu/uszoda/images/fejlec.jpg"});
+    this.items.push({title:"slide 2",color:"red", tartalom: "Ez a 2. slide tartalma"});
+    this.items.push({title:"slide 38",color:"orange",tartalom: "Ez a 3. tartalmi rész"});
 
 	console.log("carouse elemei: ",this.items.length)
 
